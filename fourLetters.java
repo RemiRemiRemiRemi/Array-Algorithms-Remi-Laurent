e38ff7a87e1a1e585723ebe4546dd473d74b1bc4
Remi <zestysourcitrus@gmail.com>
2026-01-21 12:42:00 -0500

    ';l;ljiy6ijhmjk,ytrnm, iuytrbn m

---
+++ fourLetters.java
@@ -0,0 +1,17 @@
+public class fourLetters {
+    public static void main(String[] args) {
+        String [] names = new String[] {"Jack", "Uri", "Kiana", "Kellin", "Remi"};
+
+
+        for(String name : names){
+            if(name.length() == 4){
+                System.out.println(name);
+            }
+        }
+    
+    
+
+
+
+    }
+}
